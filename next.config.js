91a0388f7d6c57ef3e05066d670cffc80a337497
vatsalassetscout-alt <vatsal.assetscout@gmail.com>
2026-06-09 17:07:53 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [], // Add any image domains if needed
-  },
-}
-
-module.exports = nextConfig
